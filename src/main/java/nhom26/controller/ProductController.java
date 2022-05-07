9709bd956ac3bdfda7b8f490d219e6fea23d91da
TaPDuy <tpduy2001@gmail.com>
2022-05-07 10:57:41 +0700

    added controllers

--- src/main/java/nhom26/controller/ProductController.java
+++ src/main/java/nhom26/controller/ProductController.java
@@ -1,15 +1,22 @@
 package nhom26.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import nhom26.model.Product;
 
 @Controller
-@RequestMapping("/products")
 public class ProductController {
 	
-	@GetMapping
+	@GetMapping("/products")
 	public String showProducts() {
 		return "products";
+	}
+	
+	@GetMapping("/updateProduct")
+	public String updateProduct(Model model) {
+		model.addAttribute("product", new Product());
+		return "update";
 	}
 }
