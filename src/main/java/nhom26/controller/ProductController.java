1481d6d72cae1a4d228a095e31b778f359ab0523
TaPDuy <tpduy2001@gmail.com>
2022-05-07 16:44:02 +0700

    fixed update and delete

--- src/main/java/nhom26/controller/ProductController.java
+++ src/main/java/nhom26/controller/ProductController.java
@@ -3,6 +3,8 @@ package nhom26.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +12,18 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.extern.slf4j.Slf4j;
 import nhom26.repository.ProductRepository;
 import nhom26.model.Product;
 
+@Slf4j
 @Controller
 @RequestMapping("products")
 public class ProductController {
@@ -41,6 +48,24 @@ public class ProductController {
 		return "add";
 	}
 	
+	@GetMapping("update/{code}")
+	public String showUpdateProduct(@PathVariable(value="code") String code, Model model) {
+		Optional<Product> product = productRepository.findById(code);
+		if (product.isPresent())
+			model.addAttribute("product", product.get());
+		
+		return "update";
+	}
+	
+	@GetMapping("delete/{code}")
+	public String showDeleteProduct(@PathVariable(value="code") String code, Model model) {
+		Optional<Product> product = productRepository.findById(code);
+		if (product.isPresent())
+			model.addAttribute("product", product.get());
+		
+		return "confirmDelete";
+	}
+	
 	@PostMapping
 	public String addProduct(@ModelAttribute Product product, Model model) {
 		Optional<Product> p = productRepository.findById(product.getCode());
@@ -55,20 +80,15 @@ public class ProductController {
 		return "redirect:/products";
 	}
 	
-	@PutMapping
-	public String updateProduct(@ModelAttribute Product product, Model model) {
-		Product p = productRepository.getById(product.getCode());
-		
-		p.setDescription(product.getDescription());
-		p.setPrice(product.getPrice());
-		productRepository.save(p);
-		
-		return "products";
+	@PostMapping("update")
+	public String updateProduct(@ModelAttribute("product") Product product, Model model) {
+		productRepository.save(product);
+		return "redirect:/products";
 	}
 	
-	@DeleteMapping
-	public String deleteProduct(@ModelAttribute Product product, Model model) {
+	@PostMapping("delete")
+	public String deleteProduct(@ModelAttribute("product") Product product, Model model) {
 		productRepository.deleteById(product.getCode());
-		return "products";
+		return "redirect:/products";
 	}
 }
