058c56dd274ea4d45744990f8e263c4a724be1b6
Kawoa NMC <manhcuong0978481376@gmail.com>
2022-05-07 10:58:06 +0700

    add get and post

--- src/main/java/nhom26/controller/ProductController.java
+++ src/main/java/nhom26/controller/ProductController.java
@@ -1,16 +1,40 @@
 package nhom26.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import nhom26.model.Product;
+import nhom26.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@GetMapping
-	public String showProducts() {
+	public String showProducts(Model model) {
+		List<Product> products = productRepository.findAll();
+		
+		model.addAttribute(products);
+		
 		return "products";
+	}
+	
+
+	@PostMapping
+	public String addProduct(Model model) {
+		Product product = new Product();
+		model.addAttribute(product);
+		
+		return "add";
 	}
 	
 	
