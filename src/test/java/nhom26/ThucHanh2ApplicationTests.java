c1a76c4b8155c16118286619ece8008b6948841d
TaPDuy <tpduy2001@gmail.com>
2022-05-07 08:23:11 +0700

    initial commit

---
+++ src/test/java/nhom26/ThucHanh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package nhom26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThucHanh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
